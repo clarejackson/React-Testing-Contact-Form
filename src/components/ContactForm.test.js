7d21ec84a6a753455283a3db711e00fc9436586e
Clare Jackson <clare-jackson@lambdastudents.com>
2020-12-10 20:37:42 -0500

    All inputs passing.

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -1,7 +1,25 @@
 import React from "react";
 import {render, screen} from "@testing-library/react";
 import ContactForm from "./ContactForm";
+import userEvent from "@testing-library/user-event";
 
 test("renders ContactForm without errors", () => {
   render(<ContactForm />);
+})
+
+test("user can fill out and submit the form", () => {
+  render(<ContactForm />);
+
+  const firstNameInput = screen.getByLabelText(/first name/i);
+  const lastNameInput = screen.getByLabelText(/last name/i);
+  const emailInput = screen.getByLabelText(/email/i);
+  const messageInput = screen.getByLabelText(/message/i);
+  
+  userEvent.type(firstNameInput, "Clare");
+  userEvent.type(lastNameInput, "Jackson");
+  userEvent.type(emailInput, "email@email.com");
+  userEvent.type(messageInput, "This my message.");
+
+  
+
 })
